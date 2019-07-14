7753162a2f996adf82c7b78f90a04cbfc8c00283
Rifky Fauzi <rifkyfauzi@Rifkys-MacBook-Air.local>
2019-07-14 22:37:28 +0700

    reupload linear model

--- final_rollwaves_linear.cpp
+++ final_rollwaves_linear.cpp
@@ -5,36 +5,31 @@
 #include<math.h>
 #include<iomanip>
 #include <algorithm>
-#include <complex> 
-#include <time.h>
 // # define M_PI           3.14159265358979323846  /* pi */
 using namespace std;
 
 float fun1(float a, float h0, float k) {
-    return(h0 * (1 + 0.005 * sin(k * a)));
+    return( (0 + 0.05 * sin(k * a)));
 }
 
 float fun2(float a, float h0, float k, float co, float F) {
-    return( sqrt(F * co * h0 * (1 + 0.005 * sin(k * a))) );
-}
-
-float fun3(float a, float h0, float u0, float rp, float tp, float k, float co, float F) {
-    return( u0 +  0.005*rp*sin(k*a + tp) );
+    return( sqrt( (0 + 0.00 * sin(k * a))) );
 }
 
 int main() {
     // double L = 1, dx = 0.000833333333333, dt = 0.000083333;
     // double L = 1, dx = 0.000833333333333, dt = 0.00046667; // 0.56dx;
-    double L = 1, dx = 0.00019, dt = dx*0.05;
+    double L = 1, dx = 0.001, dt = dx*0.5;
     // double L = 1, dx = 0.000833333333333, dt = dx*1.2;
     // double L = 1, dx = 0.000833333333333, dt = dx*0.78;
-    long int X = round(L/dx);
-    long int T = round(15/dt);
+    int X = round(L/dx);
+    int T = round(20/dt);
     double x[X-1], h[X - 1],  u[X - 1], hi[X - 1], ui[X - 1], hh[X - 1], uu[X - 1], hn[X - 1], un[X - 1], H1[X - 1], H2[X - 1], H3[X - 1], HH[X - 1], U1[X - 1], U2[X - 1], U3[X - 1], UU[X - 1], Hs[4][X - 1], Us[4][X - 1], umax;
     double ht[23][X-1],  ta, tb, tc, td;
+    double theta, co, si, u0,nu,mu;
     int n,j, vv=1;
     int pembagi = 100, maks = T/100;
-    int v = 1, skip = round( T / 2000 );
+    int v = 1, skip = round( T / 1000 );
     
     if (skip == 0){
         skip = 1;
@@ -47,86 +42,33 @@ int main() {
     
     
     
-    /////////////////////////////////////////////////// w calculation starts here
+    // time
+    int a = 3, b = round(T / 3), c = round(2 * T / 3), d = T - 1;
+    
     
     // Parameters
-    double k = 4 * M_PI, F = 9.0, Cf = 0.006, g = 9.8, q0 = 0.001, nu = 0.001;
+    double k = 2 * M_PI, F = 2 , R = 1000, Cf = 0.006, g = 9.8, h0 = 0.0042, sigma = 0.7;
+    printf("Simulasi Roll Waves hingga T = %4.2f\n", T*dt);
+    printf("k = %d pi, F = %f, R = %f, sigma = %f\n", int(k / M_PI), F, R, sigma);
     
     /// calculate several paramaters
-    double theta = atan(F * Cf), co = g * cos(theta), si = g * sin(theta);
-    
-    double h0 = pow((q0*q0/(co * F)), 0.33333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333);
-    double u0 = q0/h0;
-    
-    /// wind-related value
-    double U = 15; //wind velocity in m/s
-    double r = 1000,  ra = 1.23, s = 0.25;
-    
-    /// calculate nondimensional parameter
-    double R = u0/(nu*L), sigma = h0/(L*Cf), mu = s*(ra*h0/(r*L))*abs(U)*U/(u0*u0);
-    
-    
-    double a = pow((1/sigma + k*k/(2*R)),2) - k*k/F;
-    double b = -k/sigma + mu*k*k*k;
-    double wrp = (1/sigma + k*k/(2*R)) + sqrt(0.5*(a + sqrt(a*a + b*b)));
-    double wrm = (1/sigma + k*k/(2*R)) - sqrt(0.5*(a + sqrt(a*a + b*b)));
-    double wip = k + sqrt(0.5*( -a + sqrt(a*a + b*b) ));
-    double wim = k - sqrt(0.5*( -a + sqrt(a*a + b*b) ));
-    
-    double wa, amp;
-    if (wip < wim){
-        wa = wip/k;
-        amp = wrp;
-    }
-    else {
-        wa = wim/k;
-        amp = wrm;
-    }
-    
-    /// critical Froude number for stability of uniform flow
-    double aa = (2 + sigma*k*k/R)/(1-mu*sigma*k*k), Fc = pow( aa  , 2);
-    
-    
-    //// amplitudo and phase
-    double rp = sqrt((amp - u0)*(amp - u0) + wa*wa);
-    double tp = atan( wa / (amp - u0) );
-    printf("############################################ Physical Parameter############################## \n");
-	printf("Cf = %0.3f \n", Cf);
-    printf("Fluid depth h0 = %f \n", h0);
-    printf("Fluida velocity u0 = %f \n", u0);
-    printf("Fluid kinematic Viscosity nu = %0.3f \n", nu);
-    printf("Wind velocity U = %0.2f \n", U);
-    printf("Wave number k = %0.fpi \n", round(k/(M_PI)));
-    printf("############################################ Nondimensional Parameter############################## \n");
-    printf("Froude Number F = %0.3f \n", F);
-    printf("Wind Parameter mu = %f \n", mu);
-    printf("Reynold Number R = %f \n", R);
-    printf("Parameter sigma = %f \n", sigma);
-    printf("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\ Stability criterion F <= %f ////////////////////////////////\n", Fc);
-    printf("############################################ Monocromatic Wave ############################## \n");
-    printf("Initial fluid velocity amplitude rp = %f \n", rp);
-    printf("Initial fluid velocity phase tp = %f \n", tp);
-    printf("wave speed1 = %f \n", wip/k);
-    printf("wave speed2 = %f \n", wim/k);
-    printf("amplification_factor_1 = %f \n", wrp);
-    printf("amplification_factor_2 = %f \n", wrm);
-    printf("Please wait, I'm calculating for you ;)' \n", wrm);
-    
-    
-    
-    /////////////////////////////////////////////////// w calculation ends here
+    theta = atan(F * Cf);
+    co = g * cos(theta);
+    si = g * sin(theta);
+    u0 = sqrt( h0);
+    nu = 0; u0 / (R * L);
+    mu = 0;0.001/5; //-0.075;//0.1 ;
     
     /// initial condition
     x[0] = dt;
     for (j = 0; j <= X - 1; j++) {
         h[j] = fun1(x[j], h0,  k );
-        //u[j] = fun2(x[j], h0,  k,  co,  F);
-        u[j] = fun3(x[j], h0,  u0, rp, tp, k,  co,  F);
+        u[j] = 0.5*fun1(x[j], h0,  k ); //fun2(x[j], h0,  k,  co,  F);
     }
-    // initial wave profile
+    
     for (j = 0; j <= X - 1; j++) {
         hi[j] = fun1(x[j], h0,  k );
-        ui[j] = fun3(x[j], h0,  u0, rp, tp, k,  co,  F);
+        ui[j] = 0.5*fun1(x[j], h0,  k ); //fun2(x[j], h0,  k,  co,  F);
     }
     
     for (j = 0; j <= X - 1; j++) {
@@ -136,32 +78,29 @@ int main() {
         U2[j] = 0;
     }
     // Export Plot
-    ofstream data_plot, u_plot;
+    ofstream data_plot, umaxx;
     data_plot.open ("data_plot.csv");
-    u_plot.open ("u_plot.csv");
+    umaxx.open ("umax.csv");
     
     for (int j = 0; j <= X - 2; j++){
         data_plot << x[j] <<"," ;
     }
     data_plot << x[X - 1] <<"\n";
+    
+    for (int j = 0; j <= X - 2; j++){
+        data_plot << h[j] <<"," ;
+    }
+    data_plot << h[X - 1] <<"\n";
+    
 
-    for (int j = 0; j <= X - 2; j++){
-        data_plot << hi[j] <<"," ;
-    }
-    data_plot << hi[X - 1] <<"\n";
-     
-    for (int j = 0; j <= X - 2; j++){
-        u_plot << ui[j] <<"," ;
-    }
-    u_plot << ui[X - 1] <<"\n";
     
     /// main iteration starts
     for (n = 0; n <= T; n++) {
         
         // Predictor Operator
         for (j = 1; j <= X - 2 ; j++){
-            H3[j] = -h[j] * (u[j + 1] - u[j - 1]) / (2 * dx) - u[j] * (h[j + 1] - h[j - 1]) / (2 * dx);
-            U3[j] = -u[j] * (u[j + 1] - u[j - 1]) / (2 * dx) - co * (h[j + 1] - h[j - 1]) / (2 * dx) + (si - Cf * (u[j] * u[j]) / h[j]) + nu * (u[j + 1] - 2 * u[j] + u[j - 1]) / (dx * dx) + mu * (h[j + 1] - 2 * h[j] + h[j - 1]) / (dx * dx);
+            H3[j] = - (u[j + 1] - u[j - 1]) / (2 * dx) - (h[j + 1] - h[j - 1]) / (2 * dx);
+            U3[j] = - (u[j + 1] - u[j - 1]) / (2 * dx) - (1/F) * (h[j + 1] - h[j - 1]) / (2 * dx) + (h[j] - 2*u[j])/sigma + (1/R) * (u[j + 1] - 2 * u[j] + u[j - 1]) / (dx * dx) - mu * (h[j + 1] - 2 * h[j] + h[j - 1]) / (dx * dx);
         }
         
         // Periodic Boundary Condition
@@ -187,8 +126,8 @@ int main() {
         
         // Corrector Operator
         for (j = 1; j <= X - 2; j++) {
-            HH[j] = -hh[j] * (uu[j + 1] - uu[j - 1]) / (2 * dx) - uu[j] * (hh[j + 1] - hh[j - 1]) / (2 * dx);
-            UU[j] = -uu[j] * (uu[j + 1] - uu[j - 1]) / (2 * dx) - co * (hh[j + 1] - hh[j - 1]) / (2 * dx) +  (si - Cf * (uu[j] * uu[j]) / hh[j]) + nu * (uu[j + 1] - 2 * uu[j] + uu[j - 1]) / (dx * dx) + mu * (hh[j + 1] - 2 * hh[j] + hh[j - 1]) / (dx * dx);
+            HH[j] = - (uu[j + 1] - uu[j - 1]) / (2 * dx) - (hh[j + 1] - hh[j - 1]) / (2 * dx);
+            UU[j] = - (uu[j + 1] - uu[j - 1]) / (2 * dx) - (1/F) * (hh[j + 1] - hh[j - 1]) / (2 * dx) +  (hh[j] - 2*uu[j] ) / sigma + (1/R) * (uu[j + 1] - 2 * uu[j] + uu[j - 1]) / (dx * dx) - mu * (hh[j + 1] - 2 * hh[j] + hh[j - 1]) / (dx * dx);
         }
         
         // Periodic Boundary Condition
@@ -223,6 +162,32 @@ int main() {
             U2[j] = U3[j];
         }
         
+        // Time Capture
+        if (n == 0) {
+            //printf("n = %d\n", n);
+            for (int i = 0; i <= X - 1; i++) {
+                Hs[0][i] = hn[i];
+                Us[0][i] = un[i];
+            }
+        } else if (n == b) {
+            //printf("n = %d\n", n);
+            for (int i = 0; i <= X - 1; i++) {
+                Hs[1][i] = hn[i];
+                Us[1][i] = un[i];
+            }
+        } else if (n == c) {
+            //printf("n = %d\n", n);
+            for (int i = 0; i <= X - 1; i++) {
+                Hs[2][i] = hn[i];
+                Us[2][i] = un[i];
+            }
+        } else if (n == d) {
+            //printf("n = %d\n", n);
+            for (int i = 0; i <= X - 1; i++) {
+                Hs[3][i] = hn[i];
+                Us[3][i] = un[i];
+            }
+        }
         
         // update
         for (int i = 0; i <= X - 1; i++) {
@@ -232,28 +197,21 @@ int main() {
         
         //plot
         if ( n == skip * v ) {
-        	// fluid depth data save
             for (j = 0; j <= X - 2; j++){
                 data_plot << hn[j] <<"," ;
             }
             data_plot << hn[X - 1];
             data_plot <<"\n";
-            // fluid velocity data save
-            for (j = 0; j <= X - 2; j++){
-                u_plot << hn[j] <<"," ;
-            }
-            u_plot << hn[X - 1];
-            u_plot <<"\n";
             v++;
         }
         
         
-        //umax = *std::max_element(un, un + X - 1);
-        //umaxx << umax << "\n";
+        umax = *std::max_element(un, un + X - 1);
+        umaxx << umax << "\n";
     } //main iteartion stops
     cout<< "\n" <<endl;
     data_plot.close();
-    u_plot.close();
+    umaxx.close();
     
     // Print Results
     for (int j = 0; j <= X - 1; j++){
@@ -263,7 +221,9 @@ int main() {
     // Export Results
     ofstream data_array;
     data_array.open ("data_array.csv");
-
+    for (int j = 0; j <= X - 1; j++){
+        data_array << Hs[0][j] <<","  << Hs[1][j] <<","<< Hs[2][j] <<","<< Hs[3][j] <<"\n";
+    } // Export Results
     data_array.close();
     
     
